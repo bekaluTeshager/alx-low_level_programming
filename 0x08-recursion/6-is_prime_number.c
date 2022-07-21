db5211de3d64bee9ae69e68c34c0f6438190208c
bekalu teshager <bekaluteshager@gmail.com>
2022-07-21 01:34:22 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * helperFunction - function
+ * @num: num
+ * @i: factor
+ * Return: returns
+ */
+int helperFunction(int num, int i)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (helperFunction(num i + 1));
+		}
+	}
+}
+
+/**
+ * is_prime_number - function
+ * @n: num
+ * Return: returns
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperFunction(n, 2));
+	}
+}
