a94aceb8f3578835f2df42f481ec3d2a8041237e
bekalu teshager <bekaluteshager@gmail.com>
2022-12-21 07:41:36 -0800

    fix

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower -> function
+ * Return: returns 1
+ * @c: parameter
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - function
+ * Return: returns 1
+ * @c: int c
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+
